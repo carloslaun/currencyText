23e353b715bf0eaaaf982616ffa8b580f599c03c
carlos <carlos_cjla@hotmail.com>
2020-12-13 10:29:23 -0500

    lol

--- src/app/component/every_mundo.component.ts
+++ src/app/component/every_mundo.component.ts
@@ -10,6 +10,12 @@ import { Component, OnInit } from '@angular/core';
 export class Everymundo implements OnInit{
     entrada: number = 0;
 
+    onTextboxInput(event:any){
+        this.entrada = event.target.value;
+
+
+    }
+
   
     constructor(){
  
